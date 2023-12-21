5e52ca92c68c5dd10b43b83867cb87b4c621ff50
Tomas Brabec <xtbr123@gmail.com>
2023-12-21 21:09:59 +0100

    Added parameter processing. Using `argparse` for CLI options parsing.

--- vlog2ipxact.py
+++ vlog2ipxact.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import sys
+import pathlib
 import anytree
 import logging
 import argparse
@@ -138,6 +139,125 @@ class Port(object):
         return p;
 
 
+class Parameter(object):
+
+    attrs = ['datatype', 'dimensions', 'name', 'value'];
+
+    def __init__(self, name, **kwargs):
+        self.name = name;
+
+        for attr in Parameter.attrs:
+            if hasattr(self,attr):
+                continue;
+
+            if attr in kwargs:
+                setattr(self,attr,kwargs[attr]);
+            else:
+                setattr(self,attr,None);
+
+    def __str__(self):
+        attrs = [];
+        for attr in Parameter.attrs:
+            val = getattr(self,attr);
+            if val:
+                # dimensions need to be treated specifically as it is
+                # a list of `TypeDimension` instances
+                if attr=='dimensions':
+                    attrs.append(''.join([str(d) for d in val]));
+                elif attr=='value':
+                    attrs.append('=');
+                    attrs.append(val);
+                else:
+                    attrs.append(val);
+
+        return ' '.join(attrs);
+
+    def etXact(self):
+        p = et.Element('ipxact:port');
+
+        name = et.SubElement(p, 'ipxact:name');
+        name.text = self.name;
+
+        signal = et.SubElement(p, 'ipxact:wire');
+
+        direction = et.SubElement(signal, 'ipxact:direction');
+        if self.direction in Port.lutDirection:
+            direction.text = Port.lutDirection[self.direction];
+        else:
+            #TODO report error/warning
+            direction.text = Port.lutDirection['input'];
+
+        if self.dimensions and len(self.dimensions) > 0:
+            vectors= et.SubElement(signal, 'ipxact:vectors');
+            for dimension in self.dimensions:
+                vectors.append(dimension.etXact());
+
+        if self.datatype:
+            wiredefs = et.SubElement(signal, 'ipxact:wireTypeDefs');
+            wiredef = et.SubElement(wiredefs, 'ipxact:wireTypeDef');
+            typename = et.SubElement(wiredef, 'ipxact:typeName');
+            typename.text = self.datatype;
+
+        return p;
+
+
+def get_parameters(module_data: verible_verilog_syntax.SyntaxData):
+    params = [];
+    lastParamDecl = None;
+    for param in module_data.iter_find_all({"tag": ["kParamDeclaration"]}):
+        if param.tag == 'kParamDeclaration':
+            lastParamDecl = param;
+
+        name = param.find({"tag": ["kUnqualifiedId", "SymbolIdentifier", "EscapedIdentifier"]});
+        if name:
+            name = name.text;
+        else:
+            name = 'undefined_port';
+
+        dimensions = None;
+        datatype = None;
+        value = None;
+
+        if lastParamDecl:
+            paramDimensions = lastParamDecl.find({'tag': ['kDeclarationDimensions']});
+            if paramDimensions:
+                dimensions = [];
+                paramDimensions = paramDimensions.find_all({'tag': ['kDimensionRange','kDimensionScalar']});
+                for paramDimension in paramDimensions:
+                    if paramDimension.tag == 'kDimensionRange':
+                        dimensionRange = paramDimension.find_all({'tag':['kExpression']}, iter_ = PreOrderDepthTreeIterator, depth=1);
+                        left = dimensionRange[0].text;
+                        right = dimensionRange[1].text;
+                        dimensions.append( TypeDimension(left,right) );
+                    elif paramDimension.tag == 'kDimensionScalar':
+                        size = paramDimension.find({'tag':['kExpressionList']});
+                        if size:
+                            dimensions.append( TypeDimension('0',size.text+'-1') );
+
+            datatype = lastParamDecl.find({'tag': ['kTypeInfo']});
+            if datatype:
+                datatype = datatype.text;
+                if len(datatype) == 0:
+                    datatype = None;
+            else:
+                if hasattr(lastParamDecl.children[1], 'tag'):
+                    datatype = lastParamDecl.children[1].text;
+                else:
+                    datatype = None;
+
+            value = lastParamDecl.find({'tag': ['kTrailingAssign']});
+            if value:
+                value = value.find({'tag': ['kExpression']});
+                if value:
+                    value = value.text;
+
+        #TODO print(anytree.RenderTree(param));
+        #TODO print(param.children[0]);
+        params.append( Parameter(name, datatype=datatype, dimensions=dimensions, value=value) );
+
+    return params;
+
+
 def get_ports(module_data: verible_verilog_syntax.SyntaxData):
     ports = [];
     lastPortDecl = None;
@@ -198,30 +318,50 @@ def process_file_data(path: str, data: verible_verilog_syntax.SyntaxData):
                 name = name.text;
 
         if name:
-            print(f"[{name}]");
+            logging.debug(f"[{name}]");
 
         ports = get_ports(module);
         if ports:
             for port in ports:
-                print(f"\t{port}");
+                logging.debug(f"\t{port}");
 
-        modules.append( {'name':name, 'ports':ports} );
+        params = get_parameters(module);
+        if params:
+            for param in params:
+                logging.debug(f"\t# {param}");
+
+        modules.append( {'name':name, 'ports':ports, 'parameters':params} );
     return modules;
 
-parser = argparse.ArgumentParser(description='Creates IP-XACT 2014 file from a SystemVerilog file.')
+parser = argparse.ArgumentParser(description='Extracts SystemVerilog/Verilog module interface into IP-XACT 2014.');
 parser.add_argument('-o', '--output', dest='output', required=False, type=pathlib.Path,
-        help='IP-XACT output file, stdout if not given')
-parser.add_argument('-i', '--input', dest='infile', required=True, type=pathlib.Path,
-        help='SystemVerilog')
+        help='IP-XACT output file, stdout if not given.');
+parser.add_argument('-i', '--input', dest='file', required=True, type=pathlib.Path,
+        help='SystemVerilog/Verilog file to process.');
+parser.add_argument('--xact', dest='xact', required=False, type=pathlib.Path,
+        help='IP-XACT 2014 to be updated with module information.')
+parser.add_argument('--verible', dest='verible', required=False, type=pathlib.Path,
+        help='Path to `verible-verilog-syntax` binary.');
+parser.add_argument('--xact-library', dest='library', required=False, type=str,
+        help='IP-XACT component library name.');
+parser.add_argument('--xact-version', dest='version', required=False, type=str,
+        help='IP-XACT component version number.');
+parser.add_argument('--xact-vendor', dest='vendor', required=False, type=str,
+        help='IP-XACT component vendor name.');
 
 # parse CLI options
-opts = parser.parse_args()
+opts = parser.parse_args();
 
 # setup logging level
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG);
 
-parser_path = sys.argv[1];
-file_path = sys.argv[2];
+# `verible` parser binary
+parser_path='verible-verilog-syntax';
+if opts.verible:
+    parser_path = str(opts.verible);
+
+# input SystemVerilog/Verilog file
+file_path = str(opts.file);
 
 parser = verible_verilog_syntax.VeribleVerilogSyntax(executable=parser_path);
 try:
@@ -246,10 +386,22 @@ if len(modules) > 0:
     name = et.SubElement(comp, 'ipxact:name');
     version = et.SubElement(comp, 'ipxact:version');
 
-    vendor.text = 'vendor';
-    library.text = 'library';
-    name.text = 'name';
-    version.text = '1.0.0';
+    if opts.vendor:
+        vendor.text = opts.vendor;
+    else:
+        vendor.text = 'vendor';
+
+    if opts.library:
+        library.text = opts.library;
+    else:
+        library.text = 'library';
+
+    name.text = module['name'];
+
+    if opts.version:
+        version.text = opts.version;
+    else:
+        version.text = '1.0.0';
 
     model = et.SubElement(comp, 'ipxact:model');
 
@@ -261,6 +413,11 @@ if len(modules) > 0:
     _pretty_print(comp);
     tree = et.ElementTree(comp);
     #et.indent(tree, space="\t", level=0);
-    et.dump(tree);
-    with open('output.xml', 'wb') as f:
-        tree.write(f)
+    #et.dump(tree);
+
+    if opts.output:
+        with open(str(opts.output), 'w') as f:
+            tree.write(f, encoding='unicode', xml_declaration=True);
+    else:
+        tree.write(sys.stdout, encoding='unicode', xml_declaration=True);
+
