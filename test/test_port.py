1d3059f916861662850f7cc091a22e2e533ce93b
Tomas Brabec <xtbr123@gmail.com>
2024-01-11 18:36:11 +0100

    Tests for a single output and inout port.

--- test/test_port.py
+++ test/test_port.py
@@ -30,6 +30,57 @@ class TestVlogGetPorts(unittest.TestCase):
         data = parser.parse_string(vlogSyntax);
         return data;
 
+    def test_single_undirected_untyped_scalar(self):
+        data = '''
+        module foo(
+            a
+        );
+        endmodule
+        '''
+
+        cst = self.parse_verilog(data);
+        module = cst.tree.find({"tag": "kModuleDeclaration"});
+        ports = get_ports(module);
+
+        self.assertEqual( len(ports), 1 );
+
+        exp = {'name': 'a', 'type': None, 'dir': 'in'};
+        self.assertEqual( ports[0].toDict(), exp );
+
+    def test_single_undirected_typed_scalar(self):
+        data = '''
+        module foo(
+            logic bar
+        );
+        endmodule
+        '''
+
+        cst = self.parse_verilog(data);
+        module = cst.tree.find({"tag": "kModuleDeclaration"});
+        ports = get_ports(module);
+
+        self.assertEqual( len(ports), 1 );
+
+        exp = {'name': 'bar', 'type': 'logic', 'dir': 'in'};
+        self.assertEqual( ports[0].toDict(), exp );
+
+    def test_single_undirected_typed_vector(self):
+        data = '''
+        module foo(
+            int [2:1] bar
+        );
+        endmodule
+        '''
+
+        cst = self.parse_verilog(data);
+        module = cst.tree.find({"tag": "kModuleDeclaration"});
+        ports = get_ports(module);
+
+        self.assertEqual( len(ports), 1 );
+
+        exp = {'name': 'bar', 'type': 'int', 'dir': 'in', 'dimensions': [['2','1']]};
+        self.assertEqual( ports[0].toDict(), exp );
+
     def test_single_input_typed_scalar(self):
         data = '''
         module foo(
@@ -149,5 +200,107 @@ class TestVlogGetPorts(unittest.TestCase):
         self.assertEqual( len(ports), 1 );
 
         exp = {'name': 'a_b_c', 'type': 'logic', 'dir': 'in', 'dimensions': [['WIDTH-1','0']]};
+        self.assertEqual( ports[0].toDict(), exp );
+
+    def test_single_output_typed_scalar(self):
+        data = '''
+        module foo(
+            output logic o
+        );
+        endmodule
+        '''
+
+        cst = self.parse_verilog(data);
+        module = cst.tree.find({"tag": "kModuleDeclaration"});
+        ports = get_ports(module);
+
+        self.assertEqual( len(ports), 1 );
+
+        exp = {'name': 'o', 'type': 'logic', 'dir': 'out'};
+        self.assertEqual( ports[0].toDict(), exp );
+
+    def test_single_output_untyped_scalar(self):
+        data = '''
+        module foo(
+            output o1
+        );
+        endmodule
+        '''
+
+        cst = self.parse_verilog(data);
+        module = cst.tree.find({"tag": "kModuleDeclaration"});
+        ports = get_ports(module);
+
+        self.assertEqual( len(ports), 1 );
+
+        exp = {'name': 'o1', 'type': None, 'dir': 'out'};
+        self.assertEqual( ports[0].toDict(), exp );
+
+    def test_single_output_typed_vector_downto(self):
+        data = '''
+        module foo(
+            output byte[7:0] a
+        );
+        endmodule
+        '''
+
+        cst = self.parse_verilog(data);
+        module = cst.tree.find({"tag": "kModuleDeclaration"});
+        ports = get_ports(module);
+
+        self.assertEqual( len(ports), 1 );
+
+        exp = {'name': 'a', 'type': 'byte', 'dir': 'out', 'dimensions': [['7','0']]};
+        self.assertEqual( ports[0].toDict(), exp );
+
+    def test_single_inout_typed_scalar(self):
+        data = '''
+        module foo(
+            inout logic io
+        );
+        endmodule
+        '''
+
+        cst = self.parse_verilog(data);
+        module = cst.tree.find({"tag": "kModuleDeclaration"});
+        ports = get_ports(module);
+
+        self.assertEqual( len(ports), 1 );
+
+        exp = {'name': 'io', 'type': 'logic', 'dir': 'inout'};
+        self.assertEqual( ports[0].toDict(), exp );
+
+    def test_single_inout_untyped_scalar(self):
+        data = '''
+        module foo(
+            inout IO
+        );
+        endmodule
+        '''
+
+        cst = self.parse_verilog(data);
+        module = cst.tree.find({"tag": "kModuleDeclaration"});
+        ports = get_ports(module);
+
+        self.assertEqual( len(ports), 1 );
+
+        exp = {'name': 'IO', 'type': None, 'dir': 'inout'};
+        self.assertEqual( ports[0].toDict(), exp );
+
+    def test_single_inout_typed_vector_downto(self):
+        data = '''
+        module foo(
+            inout string[7:0] s
+        );
+        endmodule
+        '''
+
+        cst = self.parse_verilog(data);
+        module = cst.tree.find({"tag": "kModuleDeclaration"});
+        ports = get_ports(module);
+
+        self.assertEqual( len(ports), 1 );
+
+        exp = {'name': 's', 'type': 'string', 'dir': 'inout', 'dimensions': [['7','0']]};
         self.assertEqual( ports[0].toDict(), exp );
 
